9e5f0657b463559a2c6b999b612ef2786ee31dc9
Tomas Jansson <tomas.jansson@gmail.com>
2015-06-25 10:05:19 +0200

    initial commit

---
+++ SuaveTopShelfDemo/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace SuaveTopShelfDemo.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("SuaveTopShelfDemo")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("SuaveTopShelfDemo")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("68486897-cf90-4c5e-86bd-0d99e2dfa21a")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ SuaveTopShelfDemo/Program.fs
@@ -0,0 +1,69 @@
+﻿open Suave
+open Suave.Http.Successful
+open Suave.Web 
+open Topshelf
+open System
+
+[<AutoOpen>]
+module Topshelf = 
+    type ServiceName = ServiceName of string
+    type DisplayName = DisplayName of string
+    type TopshelfSpec = 
+        {
+            DisplayName: DisplayName option
+            ServiceName: ServiceName option
+            Start: (HostControl -> bool) option
+            Stop: (HostControl -> bool) option
+        }
+        static member Zero = {DisplayName = None; ServiceName = None; Start = None; Stop = None}
+
+    let toAction1 f = Action<_>(f)
+    let toFunc f = Func<_>(f)
+
+    let withDisplayName name spec = {spec with DisplayName = Some name}
+    let withServiceName name spec = {spec with ServiceName = Some name}
+    let withStart start spec = {spec with Start = Some start}
+    let withStop stop spec = {spec with Stop = Some stop}
+
+    let createHostConfigurator (DisplayName dn) (ServiceName sn) createFun (conf:HostConfigurators.HostConfigurator) =
+        let createAction = createFun |> toFunc
+        conf.SetDisplayName(dn)
+        conf.SetServiceName(sn)
+        conf.Service<ServiceControl>(createAction) |> ignore
+        
+    let service (start : HostControl -> bool) (stop : HostControl -> bool) () =
+        { new ServiceControl with
+            member x.Start hc =
+              start hc
+            member x.Stop hc =
+              stop hc }
+
+    let run spec = 
+        service spec.Start.Value spec.Stop.Value 
+        |> createHostConfigurator spec.DisplayName.Value spec.ServiceName.Value
+        |> toAction1
+        |> HostFactory.Run
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let start hc = 
+        startWebServerAsync defaultConfig (OK "Hello World from a service") 
+        |> snd
+        |> Async.StartAsTask 
+        |> ignore
+        true
+
+    let stop hc = true
+
+    TopshelfSpec.Zero 
+    |> withDisplayName (DisplayName "ServiceDisplayName")
+    |> withServiceName (ServiceName "ServiceName")
+    |> withStart start
+    |> withStop stop
+    |> run
+    |> ignore
+
+    0
+
