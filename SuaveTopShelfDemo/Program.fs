292d642dde6a62774387ddbe663e9711d7092e13
Tomas Jansson <tomas.jansson@gmail.com>
2015-06-26 22:14:12 +0200

    copied api from topshelf.fsharp

--- SuaveTopShelfDemo/Program.fs
+++ SuaveTopShelfDemo/Program.fs
@@ -4,46 +4,6 @@ open Suave.Web
 open Topshelf
 open System
 
-[<AutoOpen>]
-module Topshelf = 
-    type ServiceName = ServiceName of string
-    type DisplayName = DisplayName of string
-    type TopshelfSpec = 
-        {
-            DisplayName: DisplayName option
-            ServiceName: ServiceName option
-            Start: (HostControl -> bool) option
-            Stop: (HostControl -> bool) option
-        }
-        static member Zero = {DisplayName = None; ServiceName = None; Start = None; Stop = None}
-
-    let toAction1 f = Action<_>(f)
-    let toFunc f = Func<_>(f)
-
-    let withDisplayName name spec = {spec with DisplayName = Some name}
-    let withServiceName name spec = {spec with ServiceName = Some name}
-    let withStart start spec = {spec with Start = Some start}
-    let withStop stop spec = {spec with Stop = Some stop}
-
-    let createHostConfigurator (DisplayName dn) (ServiceName sn) createFun (conf:HostConfigurators.HostConfigurator) =
-        let createAction = createFun |> toFunc
-        conf.SetDisplayName(dn)
-        conf.SetServiceName(sn)
-        conf.Service<ServiceControl>(createAction) |> ignore
-        
-    let service (start : HostControl -> bool) (stop : HostControl -> bool) () =
-        { new ServiceControl with
-            member x.Start hc =
-              start hc
-            member x.Stop hc =
-              stop hc }
-
-    let run spec = 
-        service spec.Start.Value spec.Stop.Value 
-        |> createHostConfigurator spec.DisplayName.Value spec.ServiceName.Value
-        |> toAction1
-        |> HostFactory.Run
-
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
@@ -55,15 +15,13 @@ let main argv =
         |> ignore
         true
 
-    let stop hc = true
+    let stop hc = 
+        true
 
-    TopshelfSpec.Zero 
-    |> withDisplayName (DisplayName "ServiceDisplayName")
-    |> withServiceName (ServiceName "ServiceName")
-    |> withStart start
-    |> withStop stop
-    |> run
-    |> ignore
-
-    0
+    Service.Default 
+    |> display_name "ServiceDisplayName"
+    |> instance_name "ServiceName"
+    |> with_start start
+    |> with_stop stop
+    |> with_topshelf
 
